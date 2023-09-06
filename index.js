7f1587706f8ddf6938da514d40b72fa661546f26
Phoebe Hames <77605055+PAZHames@users.noreply.github.com>
2023-09-06 13:32:52 +0100

    added some of about and projects content - altered card css, specifically border radius

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 window.addEventListener('scroll', function() {
     const leftSidebar = document.querySelector('.left');
     const rightSidebar = document.querySelector('.right');
+    const content = document.querySelector('.content'); // Add this line
     const wrapper = document.querySelector('.wrapper');
-    const windowHeight = window.innerHeight;
 
-    const headingsHeight = wrapper.offsetHeight;
-    const sidebarHeight = windowHeight > headingsHeight ? windowHeight : headingsHeight;
+    const contentHeight = content.offsetHeight; // Add this line
+    const sidebarHeight = contentHeight; // Use contentHeight instead of windowHeight
 
     leftSidebar.style.height = sidebarHeight + 'px';
     rightSidebar.style.height = sidebarHeight + 'px';
 });
+
